633581d12e432782ace6b799b6a2cfb79861ebb1
BlazeMM <102900246+BlazeMM@users.noreply.github.com>
2022-05-26 00:02:34 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* The navigation bar */
+.navbar {
+  position: fixed; /* Set the navbar to fixed position */
+  bottom: 0; /* Position the navbar at the bottom of the page */
+  width: 100%; /* Full width */
+}
+
+/* Main content */
+.main {
+  margin-bottom: 30px; /* Add a bottom margin to avoid content overlay */
+}
